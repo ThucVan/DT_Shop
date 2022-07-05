e4e6cc7cdf7828f95a71bb97c577231d87b43430
Văn Thức <72640345+thucvan@users.noreply.github.com>
2022-07-05 15:44:10 +0700

    day_2_user&main_XML

---
+++ app/src/main/java/dt/shop/dt_shop/Activity/User.kt
@@ -0,0 +1,12 @@
+package dt.shop.dt_shop.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dt.shop.dt_shop.R
+
+class User : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+    }
+}
